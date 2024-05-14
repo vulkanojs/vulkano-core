5e74b6eedc89adce32e3bd3187a284c313068e85
Iván Meléndez <argordmel@yahoo.es>
2024-05-14 12:02:57 -0500

    [fixed] Accent to regex

--- libs/Paginate.js
+++ libs/Paginate.js
@@ -2,6 +2,39 @@ const _ = require('underscore');
 
 module.exports = {
 
+  accentToRegex(_text) {
+
+    const ACCENT_STRINGS = 'ŠŒŽšœžŸ¥µÀÁÂÃÄÅÆÇÈÉÊËẼÌÍÎÏĨÐÑÒÓÔÕÖØÙÚÛÜÝßàáâãäåæçèéêëẽìíîïĩðñòóôõöøùúûüýÿ';
+    const NO_ACCENT_STRINGS = 'SOZsozYYuAAAAAAACEEEEEIIIIIDNOOOOOOUUUUYsaaaaaaaceeeeeiiiiionoooooouuuuyy';
+
+    const from = ACCENT_STRINGS.split('');
+    const to = NO_ACCENT_STRINGS.split('');
+    const result = [];
+    let text = _text;
+
+    to.forEach( (letter, key) => {
+      const exist = result.indexOf(letter);
+      if (exist >= 0) {
+        result[exist] += from[key];
+      } else {
+        result.push(letter);
+      }
+    });
+
+    result.forEach( (rg, key) => {
+      const regex = new RegExp(`[${rg}]`);
+      text = text.replace(regex, `_${key}_`);
+    });
+
+    result.forEach( (rg, key) => {
+      const regex = new RegExp(`_${key}_`);
+      text = text.replace(regex, `[${rg}]`);
+    });
+
+    return text;
+
+  },
+
   serializeQuery(_props, _query) {
 
     const props = typeof _props === 'object'
@@ -45,11 +78,11 @@ module.exports = {
         if (type === String) {
 
           if (searchType === 'startwith' || searchType === 'start') {
-            row[item] = new RegExp(['^', Utils.accentToRegex(search), '*.'].join(''), 'i');
+            row[item] = new RegExp(['^', this.accentToRegex(search), '*.'].join(''), 'i');
           } else if (searchType === 'endwith' || searchType === 'end') {
-            row[item] = new RegExp(['.*', Utils.accentToRegex(search)].join(''), 'i');
+            row[item] = new RegExp(['.*', this.accentToRegex(search)].join(''), 'i');
           } else {
-            row[item] = new RegExp(['.*', Utils.accentToRegex(search), '*.'].join(''), 'i');
+            row[item] = new RegExp(['.*', this.accentToRegex(search), '*.'].join(''), 'i');
           }
 
         } else if (type === Number) {
